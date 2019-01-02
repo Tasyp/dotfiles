4d8ed1f31d4dd9b1e663a13a19ab4fb4ac65c692
German Ivanov <germivanov@gmail.com>
2019-01-02 16:08:32 +0200

    Update dot files

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,6 @@
 set -x PATH $PATH $HOME/.scripts
+set -x PATH $PATH /home/tasyp/.gem/ruby/2.5.0/bin
+set -x EDITOR /usr/bin/vim
 setterm -cursor on
 cat ~/.cache/wal/sequences
 eval (direnv hook fish)
